abdfc2aeca21eb949349fb5c2a22fcfd0e12fbdb
s.mitskevich <s.mitskevich@godeltech.com>
2023-10-09 10:20:38 +0300

    feature: add findAll method in ClientController

--- src/main/java/com/example/gittasktest5/controllers/ClientController.java
+++ src/main/java/com/example/gittasktest5/controllers/ClientController.java
@@ -1,4 +1,11 @@
 package com.example.gittasktest5.controllers;
 
+import com.example.gittasktest5.model.Client;
+
+import java.util.List;
+
 public class ClientController {
+    public List<Client> findAll() {
+        return null;
+    }
 }
