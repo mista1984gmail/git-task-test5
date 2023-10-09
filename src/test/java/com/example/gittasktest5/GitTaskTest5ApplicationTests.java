f368ca8340b1db4af35ba511a704e9546fb0eea6
s.mitskevich <s.mitskevich@godeltech.com>
2023-10-09 10:13:35 +0300

    Initial commit

---
+++ src/test/java/com/example/gittasktest5/GitTaskTest5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gittasktest5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTaskTest5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
