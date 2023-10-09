0cba8256826eefcd73a14f54413b9b287ff07a1b
s.mitskevich <s.mitskevich@godeltech.com>
2023-10-09 11:44:30 +0300

    feature: add findAll method in ClientService

--- src/main/java/com/example/gittasktest5/services/ClientService.java
+++ src/main/java/com/example/gittasktest5/services/ClientService.java
@@ -1,4 +1,11 @@
 package com.example.gittasktest5.services;
 
+import com.example.gittasktest5.model.Client;
+
+import java.util.List;
+
 public class ClientService {
+    public List<Client> findAll(){
+        return null;
+    }
 }
